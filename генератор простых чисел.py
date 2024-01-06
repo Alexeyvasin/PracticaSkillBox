4742e8215e105364f50a5c0430a57490648c9575
Alexey_Vasin <alexeyvasin@gmail.com>
2024-01-06 08:52:07 +0300

    refactored генератор простых чисел.py

--- генератор простых чисел.py
+++ генератор простых чисел.py
@@ -1,23 +1,21 @@
+import time
+
+
 def gen(to_max):
     x = 2
     while True:
-        if x > to_max:
-            return
-        if x == 2 or x == 3:
-            yield x
-            x += 1
+        for div in range(3, x // 2, 2):
+            if not x % div:
+                break
         else:
-            x += 1 if not x % 2 else 2
-            for div in range(3, x // 2, 2):
-                if not x % div:
-                    break
-            else:
-                if x > to_max:
-                    return
-                yield x
+            if x > to_max:
+                return
+            yield x
+        x += 1 if not x % 2 else 2
 
 
-prime_num = gen(11234567)
-
-for i in prime_num:
+start = time.time()
+primes = gen(111678)
+for i in primes:
     print(i)
+print(time.time() - start)
