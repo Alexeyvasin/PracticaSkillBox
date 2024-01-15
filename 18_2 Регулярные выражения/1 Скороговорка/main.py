6e176d9216bf81dfcdd2c603a8c0cec89d4e2676
Alexey <alexeyvasin@gmail.com>
2024-01-15 22:03:37 +0300

    added 'r' before pattern 18.2.1

--- 18_2 Регулярные выражения/1 Скороговорка/main.py
+++ 18_2 Регулярные выражения/1 Скороговорка/main.py
@@ -2,12 +2,12 @@ import re
 
 tongue_twister = ('How much wood would a woodchuck chuck if a woodchuck could chuck wood?')
 
-print(re.match('wo', tongue_twister))
-res = (re.search('wo', tongue_twister))
+print(re.match(r'wo', tongue_twister))
+res = (re.search(r'wo', tongue_twister))
 print(res.group())
 print(res.start())
 print(res.end())
-results = re.findall('wo', tongue_twister)
+results = re.findall(r'wo', tongue_twister)
 print(results)
-new_string = re.sub('wo', 'ЗАМЕНА', tongue_twister)
+new_string = re.sub(r'wo', 'ЗАМЕНА', tongue_twister)
 print(new_string)
