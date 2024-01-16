2f785f3a2eac3ebcbdbbbefc3d6f6d75a80ef905
Alexeyvasin <alexeyvasin@gmail.com>
2024-01-16 11:45:31 +0300

    done 18.3.1

--- 18_3 Регулярные выражения Шаблоны/1 Согласные/main.py
+++ 18_3 Регулярные выражения Шаблоны/1 Согласные/main.py
@@ -2,5 +2,7 @@ import re
 
 text = 'Even if they are djinns, I will get djinns that can outdjinn them.'
 
-res = re.findall(r'\b[aeiouAEIOU]\w*', text)
+res = re.findall(r'\b[aeuoiAEUOI]\w*', text)
+print(res)
+res = re.findall(r'\b[^ ,.aeuoiAEUOI]\w*', text)
 print(res)
